50352767d3098cc38e14d9dc71aa0454ac0832dd
Aadit-Nagori <aadit.n.nagori@gmail.com>
2023-09-18 23:09:19 -0400

    Initial Commit

---
+++ CruiseCtrl/CruiseCtrlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CruiseCtrlApp.swift
+//  CruiseCtrl
+//
+//  Created by Aadit Nagori on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CruiseCtrlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
