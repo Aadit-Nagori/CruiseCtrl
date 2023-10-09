2f0007b2284d84248bd96c4bb68d3a635fc060a7
Henry Webb <64293535+webbhenry1@users.noreply.github.com>
2023-10-09 14:18:36 -0400

    Firebase Connection

--- CruiseCtrl/CruiseCtrlApp.swift
+++ CruiseCtrl/CruiseCtrlApp.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import Firebase
+import FirebaseAuth
 
 @main
 struct CruiseCtrlApp: App {
+    @ObservedObject private var firebaseInit = FirebaseInitializer()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    class FirebaseInitializer: ObservableObject {
+        init() {
+            FirebaseApp.configure()
         }
     }
 }
